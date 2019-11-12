82e58c21432fa2ae36c83d454207a770c02ed149
Develeopard <frontendwebdeveleopard@gmail.com>
2019-11-12 23:50:01 +0100

    some changes made

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const links = document.querySelectorAll('.menu-list li')
+const links = document.querySelectorAll('.menu-list a')
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu-open');
-    links.forEach(link =>{
-        link.classList.toggle('li-action');
-    })
 });
